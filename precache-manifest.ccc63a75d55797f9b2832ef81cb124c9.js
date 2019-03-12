c74e98c441597b9e8078dda212557e88356f6322
Carlos Yslas <cyslas@newstore.com>
2019-03-12 17:49:02 +0100

    Updates

--- precache-manifest.ccc63a75d55797f9b2832ef81cb124c9.js
+++ precache-manifest.ccc63a75d55797f9b2832ef81cb124c9.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/minesweeper/static/js/runtime~main.dd26f679.js"
   },
   {
-    "revision": "dee8acddbfa7bddf3f22",
-    "url": "/minesweeper/static/js/main.88315d6f.chunk.js"
+    "revision": "e6ee1d2bc540bbc41a3d",
+    "url": "/minesweeper/static/js/main.c20e9484.chunk.js"
   },
   {
     "revision": "6b82c5d0b1e623e28259",
     "url": "/minesweeper/static/js/2.ba853b34.chunk.js"
   },
   {
-    "revision": "dee8acddbfa7bddf3f22",
+    "revision": "e6ee1d2bc540bbc41a3d",
     "url": "/minesweeper/static/css/main.af73f4c0.chunk.css"
   },
   {
-    "revision": "1a289bfbe07587e2d532174b9e732bbc",
+    "revision": "f0fcd6adceee7392463950e9a8816ebe",
     "url": "/minesweeper/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/minesweeper/precache-manifest.5d68f6185ebc20af61e296f1d4cd8615.js"
+  "/minesweeper/precache-manifest.ccc63a75d55797f9b2832ef81cb124c9.js"
 );
 
 workbox.clientsClaim();
