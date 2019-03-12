49db4be351095d6e56446b80f9dc80cb8d1adc16
Carlos Yslas <cyslas@newstore.com>
2019-03-12 16:15:14 +0100

    Updates

---
+++ precache-manifest.1d1356ec6fc4046a2d12458b26d274ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ddbf8222c3be315f0ea8",
+    "url": "/static/js/main.ee96b05f.chunk.js"
+  },
+  {
+    "revision": "3a4c98a91533336df1fe",
+    "url": "/static/js/2.f0c54cd3.chunk.js"
+  },
+  {
+    "revision": "ddbf8222c3be315f0ea8",
+    "url": "/static/css/main.af73f4c0.chunk.css"
+  },
+  {
+    "revision": "38a501e21e8e6f48eb29ef240885c1c4",
+    "url": "/index.html"
+  }
+];
